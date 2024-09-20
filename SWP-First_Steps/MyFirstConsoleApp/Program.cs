f53a9537b2e8e61ebbbceaa5f9c42bcc040bd63b
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2024-09-20 10:10:36 +0200

    first_commit

---
+++ SWP-First_Steps/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hell SWP!");
+
+string username;
+Console.Write("Enter your name: ");
+username = Console.ReadLine();
+Console.WriteLine("Your name is " + username);
+
+int age = 0;
+Console.Write("Enter your age: ");
+age = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Your age is " + age);
+if (age >= 15) 
+{
+    Console.WriteLine("You are old enough.");
+} else
+{ 
+    Console.WriteLine("You are to young.");
+};
