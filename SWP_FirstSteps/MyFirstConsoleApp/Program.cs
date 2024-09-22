d7eee6ef1b613f030d162a16fa23c3b32befb86b
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2024-09-22 14:41:24 +0200

    Programmierauftrag_01

---
+++ SWP_FirstSteps/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, SWP!");
+
+
+/*
+ * Schreiben sie eine C# Konsolenanwendung welche
+ * _ Eine Eingabe über die Tastatur einliest 
+ * _ mit <Enter> soll die Eingabe "abgeschlossen" werden und der eingegebene String auf der Konsole ausgegeben werden
+ * _ Bei Eingabe von "Beenden" soll das Programm beendet werden
+ */
+
+string userinput = " ";
+
+while (userinput != "Beenden")
+{
+    Console.Write("Bitte gebe eine Zeichenfolge ein oder Beenden um das Programm zu beenden: ");
+    userinput = Console.ReadLine();
+    Console.WriteLine("Die eingegebene Zeichenfolge lautet: " + userinput);
+}
