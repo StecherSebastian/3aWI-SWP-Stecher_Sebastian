2921706f20c49abc92b1d7eed9427fc03cb5d8cd
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2024-10-04 12:01:27 +0200

    Programmierauftrag_03

--- SWP_FirstSteps/MyFirstConsoleApp/Program.cs
+++ SWP_FirstSteps/MyFirstConsoleApp/Program.cs
@@ -9,14 +9,15 @@
  * _ mit <Enter> soll die Eingabe "abgeschlossen" werden und der eingegebene String auf der Konsole ausgegeben werden
  * _ Bei Eingabe von "Beenden" soll das Programm beendet werden
  */
+Console.WriteLine("\n Programmierauftrag_01");
 
-string? userinputString = " ";
+string? userInputString = " ";
 
-while (userinputString != "Beenden")
+while (userInputString != "Beenden")
 {
     Console.Write("Bitte gebe eine Zeichenfolge ein oder Beenden um das Programm zu beenden: ");
-    userinputString = Console.ReadLine();
-    Console.WriteLine("Die eingegebene Zeichenfolge lautet: " + userinputString);
+    userInputString = Console.ReadLine();
+    Console.WriteLine("Die eingegebene Zeichenfolge lautet: " + userInputString);
 }
 
 
@@ -28,23 +29,67 @@ while (userinputString != "Beenden")
  * Verschaffen Sie sich zuerst einen Überblick über die elementaren Datentypen von C# 
  * und wie sie diese "umwandeln" (konvertieren) können. 
  */
+Console.WriteLine("\n Programmierauftrag_02");
 
-userinputString = " ";
-int userinputInt = 0;
+userInputString = " ";
+int userInputInt = 0;
 
 while (true)
 {
     Console.Write("Bitte gib eine beliebige Zahl ein: ");
 
-    userinputString = Console.ReadLine();
+    userInputString = Console.ReadLine();
 
-    if (userinputString == "Beenden")
+    if (userInputString == "Beenden")
     {
         break;
     }
 
-    int.TryParse(userinputString, out userinputInt);
+    int.TryParse(userInputString, out userInputInt);
 
-    Console.WriteLine("Die eingegebene Zahl lautet: " + userinputInt);
-    Console.WriteLine("Die eingegebene Zahl verdoppelt lautet: " + (userinputInt * 2));
+    Console.WriteLine("Die eingegebene Zahl lautet: " + userInputInt);
+    Console.WriteLine("Die eingegebene Zahl verdoppelt lautet: " + (userInputInt * 2));
+}
+
+/* Erweitern Sie ihre Konsolenanwendung in der Art, dass nach dem Einlesen der Natürlichen Zahl eine Abfrage erscheint,
+ * in der Sie eine Mathematische Operation auswählen können...
+ * Dies kann zum Beispiel so ausschauen:
+ * Geben Sie eine Zahl zwischen 1 und 3 ein um eine Mathematische Operation auszuwählen:
+ * 1) Quadrat
+ * 2) Wurzel
+ * 3) Fakultät
+ */
+Console.WriteLine("\n Programmierauftrag_04");
+
+userInputString = " ";
+userInputInt = 0;
+
+while (!String.Equals(userInputString, "Beenden", StringComparison.CurrentCultureIgnoreCase))
+{
+    Console.Write("Bitte geben Sie eine Natürliche Zahl ein (oder Beenden um zu beenden):");
+    userInputString = Console.ReadLine();
+    if (int.TryParse(userInputString, out userInputInt))
+    {
+        Console.Write("Geben Sie eine Zahl zwischen 1 und 3 ein, um eine mathematische Operation für die vorher eingegebene Zahl zu wählen: \n 1) Quadrat \n 2) Wurzel \n 3) Fakultät \n Zahl: ");
+        userInputString = Console.ReadLine();
+        switch (userInputString)
+        {
+            case "1":
+                userInputInt *= userInputInt;
+                Console.WriteLine($"Quadrat der eingegebenen Zahl: {userInputInt}");
+                break;
+            case "2":
+                userInputInt = (int)Math.Sqrt(userInputInt);
+                Console.WriteLine($"Wurzel der eingegebenen Zahl: {userInputInt}");
+                break;
+            case "3":
+                int userInputFakultaet = 1;
+                for (int i = 2; i <= userInputInt; i++)
+                {
+                    userInputFakultaet = userInputFakultaet * i;
+                }
+                Console.WriteLine($"Fakultät der eingegebenen Zahl: {userInputFakultaet}");
+                break;
+        }
+    }
 }
