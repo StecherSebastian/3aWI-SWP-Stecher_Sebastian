ecedc8cb697a0219f9cf02d25a3c13439fc7ab5e
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2024-11-08 13:22:02 +0100

    MyFirstCat

---
+++ SWP_FirstSteps/MyFirstCat/Cat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MyFirstCat
+{
+    internal class Cat
+    {
+        private string? _Color = null;
+        private DateTime _BirthDate;
+        public string Color
+        {
+            get { return _Color; }
+            set
+            {
+                if (_Color == value) return;
+                _Color = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return (DateTime.Now.Year - _BirthDate.Year);
+            }
+        }
+        public Cat(DateTime birthdate)
+        {
+            _BirthDate = birthdate;
+        }
+    }
+}
---
+++ SWP_FirstSteps/MyFirstCat/Program.cs
@@ -0,0 +1,7 @@
+﻿using MyFirstCat;
+using System.Runtime.InteropServices.JavaScript;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Cat myCat = new Cat(new DateTime(1800, 8, 14)); //erstellt eine Instanz der Klasse Cat und weißt sie der Variable mit dem Namen myCat zu
+myCat.Color = "black";
+Console.WriteLine($"The color of my Cat is {myCat.Color} and is {myCat.Age} years old");
