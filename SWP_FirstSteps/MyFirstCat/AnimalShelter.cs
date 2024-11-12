05676723fd1933fb6f3511c1fe01a40e6d3a6eaf
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2024-11-12 10:28:30 +0100

    addedAnimalShelter

---
+++ SWP_FirstSteps/MyFirstCat/AnimalShelter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCat
+{
+    internal class AnimalShelter
+    {
+        public List<Cat> _Animals;
+        public List<Cat> Animals
+        {
+            get { return _Animals; }
+            set
+            {
+                if (_Animals == value) return;
+                _Animals = value;
+            }
+        }
+        public AnimalShelter()
+        {
+            Animals = new List<Cat>();
+        }
+    }
+}
--- SWP_FirstSteps/MyFirstCat/Program.cs
+++ SWP_FirstSteps/MyFirstCat/Program.cs
@@ -1,7 +1,35 @@
 ﻿using MyFirstCat;
-using System.Runtime.InteropServices.JavaScript;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
-Cat myCat = new Cat(new DateTime(1800, 8, 14)); //erstellt eine Instanz der Klasse Cat und weißt sie der Variable mit dem Namen myCat zu
-myCat.Color = "black";
-Console.WriteLine($"The color of my Cat is {myCat.Color} and is {myCat.Age} years old");
+string? userInputString = " ";
+DateTime userInputDate = new DateTime(0001, 01, 01);
+AnimalShelter shelterDornbirn = new AnimalShelter();
+
+while (!String.Equals(userInputString, "Exit", StringComparison.CurrentCultureIgnoreCase))
+{
+    Console.Write("\nPossible Options:\n 1) Create Cat\n 2) See Content Animal Shelter\n or Exit\n*:");
+    userInputString = Console.ReadLine();
+    switch (userInputString)
+    {
+        case "1":
+            Console.Write("\nWhen was the cat born? (YYYY/MM/DD)\n*:");
+            userInputString = Console.ReadLine();
+            while (!DateTime.TryParse(userInputString, out userInputDate))
+            {
+                Console.Write("\n### Invalid Date: Please Try Again ###\n*:");
+                userInputString = Console.ReadLine();
+            }
+            Cat myCat = new Cat(userInputDate);
+            Console.Write("\nWhich color has the cat?\n*:");
+            userInputString = Console.ReadLine();
+            myCat.Color = userInputString;
+            shelterDornbirn.Animals.Add(myCat);
+            break;
+        case "2":
+            Console.WriteLine("\nThe Shelter in Dornbirn contains:");
+            foreach (var animal in shelterDornbirn.Animals)
+            {
+                Console.WriteLine($"A {animal.Color} cat that is {animal.Age} years old");
+            }
+            break;
+    }
+}
