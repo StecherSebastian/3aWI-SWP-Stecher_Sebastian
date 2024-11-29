bc004178271d9a88ba3f89c8851cf22e60c5a645
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2024-11-29 11:43:45 +0100

    movedAttributesToFittingClass

--- SWP_FirstSteps/MyFirstCat/Cat.cs
+++ SWP_FirstSteps/MyFirstCat/Cat.cs
@@ -4,20 +4,7 @@ namespace MyFirstCat
 {
     class Cat : Animal
     {
-        private string? _Color;
-        public string? Color
-        {
-            get { return _Color; }
-            set
-            {
-                if (_Color == value) return;
-                _Color = value;
-            }
-        }
-        public Cat(DateTime birthdate, string name = "not defiend", string color = "nothing") : base(name, birthdate)
-        {
-            Color = color;
-        }
+        public Cat(DateTime birthdate, string name = "not defiend", string color = "nothing") : base(name, color, birthdate) {}
         public override string ToString()
         {
             return $"Name: {Name} Color: {Color} Age: {Age}";
