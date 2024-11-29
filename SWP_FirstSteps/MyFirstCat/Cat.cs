a10c93bfb55a0ac3c3c9b01e8d0694ca050ee1fc
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2024-11-29 15:51:40 +0100

    addedLegsAndSpeakMethod

--- SWP_FirstSteps/MyFirstCat/Cat.cs
+++ SWP_FirstSteps/MyFirstCat/Cat.cs
@@ -4,10 +4,14 @@ namespace MyFirstCat
 {
     class Cat : Animal
     {
-        public Cat(DateTime birthdate, string name = "not defiend", string color = "nothing") : base(name, color, birthdate) {}
+        public Cat(DateTime birthdate, string name = "not defiend", string color = "nothing", int legs = 4) : base(name, color, birthdate, legs) {}
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name}: *miau*");
+        }
         public override string ToString()
         {
-            return $"Name: {Name} Color: {Color} Age: {Age}";
+            return $"{Name} is {Color}, is {Age} years old and walks on {Legs} Legs.";
         }
     }
 }
